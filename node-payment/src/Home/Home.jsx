9e4d20cb39d4cc9f6db0fc2290810391ec1bee62
synysterR3v <saepuladi4@gmail.com>
2023-11-28 09:53:27 +0700

    ok

--- node-payment/src/Home/Home.jsx
+++ node-payment/src/Home/Home.jsx
@@ -1,11 +1,12 @@
 import { Box, Button, TextField } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 
 const Home = () => {
-    const [name, setName] = useState('');
-    const [order_id, setOrder_id] = useState('');
+    const [name, setName] = useState("");
+    const [order_id, setOrder_id] = useState("");
     const [total, setTotal] = useState(0);
+    const [token, setToken] = useState("");
 
     const process = async () => {
         const data = {
@@ -22,8 +23,51 @@ const Home = () => {
 
         const response = await axios.post("http://localhost:1000/api/payment/process-transaction", data, config)
 
-        console.log(response)
+        setToken(response.data.token);
     };
+
+    useEffect(()=> {
+        if (token) {
+            window.snap.pay(token, {
+                onSuccess: (result) => {
+                    localStorage.setItem("Pembayaran", JSON.stringify(result))
+                    setToken("");
+                },
+                onPending: (result) => {
+                    localStorage.setItem("Pembayaran", JSON.stringify(result))
+                    setToken("");
+                },
+                onError: (result) => {
+                    console.log(error)
+                    setToken("");
+                },
+                onClose: (result) => {
+                    console.log("Anda Belum Menyelesaikan Pembayaran")
+                    setToken("");
+                }
+
+            })
+
+            setName("");
+            setOrder_id("");
+            setTotal("");
+        }
+    }, [token])
+
+    useEffect(() => {
+        const midtransUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
+        let scriptTag = document.createElement("script")
+        scriptTag.src = midtransUrl
+
+        const midtransClientKey = "SB-Mid-client-Yf_Ff27MJAT4sAKU";
+        scriptTag.setAttribute("data-client-key", midtransClientKey)
+
+        document.body.appendChild(scriptTag)
+
+        return ()=>{
+            document.body.removeChild(scriptTag)
+        }
+    })
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh', width: '50vw', padding: 4 }}>
